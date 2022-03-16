0c4af0357af114921facfda92b504bbfed8e9d90
Jeffrey Alex Vaughn <vaughnj29@gmail.com>
2022-03-16 07:55:18 -0700

    adding context and auth

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import "./button.styles.scss";
 
-import './button.styles.scss';
+export const BUTTON_TYPE_CLASSES = {
+  google: "google-sign-in",
+  inverted: "inverted",
+};
 
-const BUTTON_TYPE_CLASSES = {
-    google: 'google-sign-in',
-    inverted: 'inverted',
-}; 
-
-const Button = ({children, buttonType, ...otherProps }) => {
-    return <button className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]} `}{...otherProps}>{children}</button>
+const Button = ({ children, buttonType, ...otherProps }) => {
+  return (
+    <button
+      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
