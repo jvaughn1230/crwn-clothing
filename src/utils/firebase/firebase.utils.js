f1759c47333713157ab784b0aabf951238daf55a
Jeffrey Alex Vaughn <vaughnj29@gmail.com>
2022-03-25 04:45:40 -0700

    adding database of items and creating nested routes for shop categories

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import { initializeApp} from 'firebase/app';
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs } from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyBUOVsjZIddGdiwThoh0Vemk3NsWGSOdqM",
@@ -25,6 +25,33 @@ const firebaseConfig = {
   export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
   export const db = getFirestore();
+
+  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+      const collectionRef = collection(db, collectionKey);
+      const batch = writeBatch(db);
+
+      objectsToAdd.forEach((object)=>{
+          const docRef = doc(collectionRef, object.title.toLowerCase());
+          batch.set(docRef, object);
+      });
+
+      await batch.commit();
+      console.log('Done');
+  }
+
+  export const getCategoriesAndDocuments = async () => {
+      const collectionRef = collection(db, 'categories');
+      const q = query(collectionRef);
+      const querySnapshot = await getDocs(q);
+
+      const categoryMap = querySnapshot.docs.reduce((acc, docSnapShot)=>{
+          const { title, items } = docSnapShot.data();
+          acc[title.toLowerCase()] = items;
+          return acc;
+      }, {});
+      return categoryMap;
+
+  }
 
   export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
     if (!userAuth) return; 
