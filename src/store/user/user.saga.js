43823ffd0c3603ed4f478ead20bba5f82fdb8867
Jeffrey Alex Vaughn <vaughnj29@gmail.com>
2022-04-26 07:31:46 -0700

    migrated sign-up to saga

--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -2,13 +2,19 @@ import { takeLatest, put, all, call } from 'redux-saga/effects';
 
 import { USER_ACTION_TYPES } from './user.types';
 
-import { signInSuccess, signInFailed } from './user.action';
+import { 
+    signInSuccess, 
+    signInFailed, 
+    signUpSuccess,
+    signUpFailed,
+} from './user.action';
 
 import { 
     getCurrentUser, 
     createUserDocumentFromAuth, 
     signInWithGooglePopup, 
-    signInAuthUserWithEmailAndPassword 
+    signInAuthUserWithEmailAndPassword,
+    createAuthUserWithEmailAndPassword 
 } from '../../utils/firebase/firebase.utils';
 
 export function* getSnapshopFromUserAuth(userAuth, additonalDetails){
@@ -52,6 +58,19 @@ export function* isUserAuthenticated(){
     }
 }
 
+export function* signUp({payload: { email, password, displayName }}){
+    try{
+        const { user } = yield call(createAuthUserWithEmailAndPassword, email, password);
+        yield put(signUpSuccess(user, { displayName }));
+    }catch(error){
+        yield put(signUpFailed(error));
+    }
+}
+
+export function* signInAfterSignUp({payload: { user, additonalDetails }}){
+    yield call(getSnapshopFromUserAuth, user, additonalDetails);
+}
+
 export function* onGoogleSignInStart(){
     yield takeLatest(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
@@ -64,10 +83,20 @@ export function* onEmailSignInStart(){
     yield takeLatest(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, signInWithEmail);
 }
 
+export function* onSignUpStart(){
+    yield takeLatest(USER_ACTION_TYPES.SIGN_UP_START, signUp);
+}
+
+export function* onSignUpSuccess(){
+    yield takeLatest(USER_ACTION_TYPES.SIGN_UP_SUCCESS, signInAfterSignUp );
+}
+
 export function* userSagas(){
     yield all([
         call(onCheckUserSession), 
         call(onGoogleSignInStart), 
-        call(onEmailSignInStart)
+        call(onEmailSignInStart),
+        call(onSignUpStart),
+        call(onSignUpSuccess)
     ]);
 }
