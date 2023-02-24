47a5996f78b9f16c7dc18af31b56e4384e8e4270
Alex <jvaughn1230@gmail.com>
2023-02-24 13:59:24 -0800

    updated packages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store, persistor } from './store/store';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Elements } from '@stripe/react-stripe-js';
-import { stripePromise } from './utils/firebase/stripe/stripe.utils';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store, persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
+import { stripePromise } from "./utils/firebase/stripe/stripe.utils";
 
-import App from './App';
+import App from "./App";
 
-import './index.scss';
+import "./index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -23,5 +23,5 @@ ReactDOM.render(
       </PersistGate>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,26 +1,26 @@
-import { useEffect } from 'react';
-import {Routes, Route} from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-import CategoriesPreview from '../categories-previw/categories-preview.component';
-import Category from '../category/category.component';
-import { fetchCategoriesStart } from '../../store/categories/category.action';
+import CategoriesPreview from "../categories-previw/categories-preview.component";
+import Category from "../category/category.component";
+import { fetchCategoriesStart } from "../../store/categories/category.action";
 
 //import './shop.styles.scss';
 
 const Shop = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(fetchCategoriesStart());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchCategoriesStart());
+  }, [dispatch]);
 
-    return(
-       <Routes>
-           <Route index element = {<CategoriesPreview />} />
-           <Route path=':category' element={<Category />} />
-       </Routes>
-    );
+  return (
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
+  );
 };
 
 export default Shop;
